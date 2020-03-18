7d44a27550ae7e769a36b44534689faeea4edf19
Michael Milton <ttmigueltt@gmail.com>
2020-03-18 17:54:40 +1100

    Improve module structure and documentation

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # LastPdfPage
 
-For each page number in a PDF, selects only the last page with that number, and then regenerates the PDF
+Sometimes PDFs are generated for a powerpoint presentation, which don't work so well as PDF documents.
+
+For example, a single slide in the presentation might be designed to show each dotpoint when the presenter clicks.
+However, once this presentation is converted to PDF, the document is unusable because each dotpoint now has its own 
+slide.
+
+`LastPdfPage` eliminates this problem by editing a PDF that has multiple pages with the same page number, and selecting
+only the last (or first) page from each page range that has this issue.
 
 # Installation
 
@@ -10,10 +17,26 @@ Simply run:
 pip install git+github.com:TMiguelT/LastPdfPage.git
 ```
 
-# Usage
+# CLI
+```
+Usage: last-pdf-page [OPTIONS] PDF
 
-Once installed, run:
+Options:
+  --select [first|last]  Whether to choose the first, or last page for each
+                         number
 
+  --help                 Show this message and exit.
+```
+
+For example, to edit a PDF and take only the last page of each page-range with the same number:
 ```bash
-last-pdf-page somePdf.pdf > fixed.pdf
+last-pdf-page --select last somePdf.pdf > fixed.pdf
+```
+
+# Python API
+```python
+from last_pdf_page import simplify_pdf
+
+with open('fixed.pdf', 'wb') as fp:
+    simplify_pdf('somePdf.pdf', select='last', outfile=fp)
 ```
--- last_pdf_page/cli.py
+++ last_pdf_page/cli.py
@@ -1,72 +1,12 @@
 import click
-import pdfrw
-import typing
 import sys
 
-
-def build_page_lookup(labels) -> typing.List[typing.Tuple[int, dict]]:
-    """
-    Given a PDF label tree (aka the value of pdf['/Root']['/PageLabels']['/Nums']), build it into an array of (index,
-    dictionary) tuples
-    """
-    lookup = []
-    it = iter(labels)
-    while True:
-        try:
-            # The PageLabels tree alternates between the page index that begins the current range, and a dictionary that
-            # describes the numbering within that range
-            range_start = next(it)
-            range_dict = next(it)
-            lookup.append((int(range_start), dict(range_dict)))
-        except StopIteration:
-            return lookup
-
-
-def simplify_pdf(pdf: str, select: str, outfile: typing.TextIO):
-    """
-    Given a PDF and a metric by which to select the first or last page in a range of pages with the same name,
-    remove the unnecessary pages
-    """
-    in_pdf = pdfrw.PdfFileReader(pdf)
-    out_pdf = pdfrw.PdfFileWriter(trailer=in_pdf)
-    for index in calculate_pages(in_pdf, select):
-        out_pdf.addPage(in_pdf.getPage(index))
-    out_pdf.write(outfile)
-
-
-def calculate_pages(pdf: pdfrw.PdfFileReader, select: str) -> typing.List[int]:
-    """
-    Given a PDF and a metric by which to select the first or last page in a range of pages with the same name,
-    produce a list of page indices
-    """
-    # The page indexes we want to output
-    final_pages = []
-
-    # Note: this is based on section 8.3.1 Page Labels of the PDF spec
-    page_lookup = build_page_lookup(pdf['/Root']['/PageLabels']['/Nums'])
-    for i, (range_start, range_dict) in enumerate(page_lookup):
-
-        # Calculate the starting index of the next section (or the last index if this is the last section)
-        next_start = page_lookup[i + 1][0] if i + 1 < len(page_lookup) else len(pdf.pages)
-
-        if '/S' in range_dict:
-            # If there is a numbering style for this range, then each page in the range is unique, and we should
-            # use all of them
-            final_pages += list(range(range_start, next_start))
-        else:
-            # If there is no numbering style for this range, each page in the range has the same number, so we
-            # have to choose either the first or last page in that range
-            if select == 'first':
-                final_pages.append(range_start)
-            else:
-                final_pages.append(next_start - 1)
-
-    return final_pages
+from last_pdf_page import simplify_pdf
 
 
 @click.command()
 @click.argument('pdf', type=click.Path())
-@click.option('--select', type=click.Choice(['first', 'last']), default='last', help='Whether to choose the first, or'
+@click.option('--select', type=click.Choice(['first', 'last']), default='last', help='Whether to choose the first, or '
                                                                                      'last page for each number')
 def main(pdf: str, select: str):
     simplify_pdf(pdf, select, sys.stdout.buffer)
